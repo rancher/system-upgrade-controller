eead3deaa34e7c84660d23a59fd7e8b627a9e6d5
Menna <menna.elmasry@rancher.com>
2020-06-09 18:14:44 +0200

    add upgrade required tag, update wrangler version, create plan test
    go fmt
    
    updated test
    
    go fmt

---
+++ e2e/suite/plan_create_test.go
@@ -0,0 +1,25 @@
+package suite_test
+
+import (
+	"github.com/rancher/system-upgrade-controller/e2e/framework"
+	upgradeapiv1 "github.com/rancher/system-upgrade-controller/pkg/apis/upgrade.cattle.io/v1"
+)
+
+var _ = Describe("Plan Creation", func() {
+	e2e := framework.New("create")
+	When("missing upgrade field", func() {
+		var (
+			err  error
+			plan *upgradeapiv1.Plan
+		)
+		BeforeEach(func() {
+			plan = e2e.NewPlan("upgrade", "", nil)
+
+		})
+		It("should return an error if upgrade in nil", func() {
+			plan.Spec.Upgrade = nil
+			plan, err = e2e.CreatePlan(plan)
+			Expect(err).Should(HaveOccurred())
+		})
+	})
+})
