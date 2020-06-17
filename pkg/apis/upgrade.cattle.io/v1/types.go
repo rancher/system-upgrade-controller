53fcd3b5350241ddc97812b97ff76275b8a09f65
Menna <menna.elmasry@rancher.com>
2020-06-17 02:32:49 +0200

    add SkipWaitForDeleteTimeout

--- pkg/apis/upgrade.cattle.io/v1/types.go
+++ pkg/apis/upgrade.cattle.io/v1/types.go
@@ -64,12 +64,13 @@ type ContainerSpec struct {
 
 // DrainSpec encapsulates `kubectl drain` parameters minus node/pod selectors.
 type DrainSpec struct {
-	Timeout          *time.Duration `json:"timeout,omitempty"`
-	GracePeriod      *int32         `json:"gracePeriod,omitempty"`
-	DeleteLocalData  *bool          `json:"deleteLocalData,omitempty"`
-	IgnoreDaemonSets *bool          `json:"ignoreDaemonSets,omitempty"`
-	Force            bool           `json:"force,omitempty"`
-	DisableEviction  bool           `json:"disableEviction,omitempty"`
+	Timeout                  *time.Duration `json:"timeout,omitempty"`
+	GracePeriod              *int32         `json:"gracePeriod,omitempty"`
+	DeleteLocalData          *bool          `json:"deleteLocalData,omitempty"`
+	IgnoreDaemonSets         *bool          `json:"ignoreDaemonSets,omitempty"`
+	Force                    bool           `json:"force,omitempty"`
+	DisableEviction          bool           `json:"disableEviction,omitempty"`
+	SkipWaitForDeleteTimeout int            `json:"skipWaitForDeleteTimeout,omitempty"`
 }
 
 // SecretSpec describes a secret to be mounted for prepare/upgrade containers.
