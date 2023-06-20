74d55b4bc4c620af73d2e874399c323f21b704bb
Connor Kuehl <connor.kuehl@suse.com>
2023-06-20 13:27:57 -0500

    Allow Plan to specify Job deadline
    If a workload is known to be slow in excess of the
    system-upgrade-controller deployment's default deadline, allow the Plan
    creator to specify a job deadline with the plan so that the entailing
    job may run to completion.
    
    Signed-off-by: Connor Kuehl <connor.kuehl@suse.com>

--- pkg/upgrade/job/job.go
+++ pkg/upgrade/job/job.go
@@ -41,6 +41,17 @@ var (
 		return defaultValue
 	}(defaultActiveDeadlineSeconds)
 
+	ActiveDeadlineSecondsMax = func(defaultValue int64) int64 {
+		if str, ok := os.LookupEnv("SYSTEM_UPGRADE_JOB_ACTIVE_DEADLINE_SECONDS_MAX"); ok {
+			if i, err := strconv.ParseInt(str, 10, 64); err != nil {
+				logrus.Errorf("failed to parse $%s: %v", "SYSTEM_UPGRADE_JOB_ACTIVE_DEADLINE_SECONDS_MAX", err)
+			} else {
+				return i
+			}
+		}
+		return defaultValue
+	}(0 /* no maximum */)
+
 	BackoffLimit = func(defaultValue int32) int32 {
 		if str, ok := os.LookupEnv("SYSTEM_UPGRADE_JOB_BACKOFF_LIMIT"); ok {
 			if i, err := strconv.ParseInt(str, 10, 32); err != nil {
@@ -327,8 +338,20 @@ func New(plan *upgradeapiv1.Plan, node *corev1.Node, controllerName string) *bat
 		),
 	}
 
-	if ActiveDeadlineSeconds > 0 {
-		job.Spec.ActiveDeadlineSeconds = &ActiveDeadlineSeconds
+	activeDeadlineSeconds := ActiveDeadlineSeconds
+
+	if plan.Spec.JobActiveDeadlineSecs > 0 {
+		activeDeadlineSeconds = plan.Spec.JobActiveDeadlineSecs
+	}
+
+	// If configured with a maximum deadline via "SYSTEM_UPGRADE_JOB_ACTIVE_DEADLINE_SECONDS_MAX",
+	// clamp the Plan's given deadline to the maximum.
+	if ActiveDeadlineSecondsMax > 0 && activeDeadlineSeconds > ActiveDeadlineSecondsMax {
+		activeDeadlineSeconds = ActiveDeadlineSecondsMax
+	}
+
+	if activeDeadlineSeconds > 0 {
+		job.Spec.ActiveDeadlineSeconds = &activeDeadlineSeconds
 		if drain != nil && drain.Timeout != nil && drain.Timeout.Milliseconds() > ActiveDeadlineSeconds*1000 {
 			logrus.Warnf("drain timeout exceeds active deadline seconds")
 		}
